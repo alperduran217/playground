65b45ef39313f74a790c7a71d6c05b2f341c9d0c
alperduran217 <alperduran217@gmail.com>
2016-05-02 21:20:43 +0300

    AnimalsENViewController comments are added

--- Playground/AnimalsENViewController.swift
+++ Playground/AnimalsENViewController.swift
@@ -12,24 +12,23 @@ import AVFoundation
 
 class AnimalsENViewController: UIViewController {
     
-//  PRE-LOAD
-    
+// !!== PRE-LOAD == !!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        let image = UIImage(named: picsArray[Int(randomNumber)]) as UIImage? // takes random image from array
+
         
-        let image = UIImage(named: picsArray[Int(randomNumber)]) as UIImage?
+        button.setImage(image, forState: .Normal) // sets button to that random image
         
-        button.setImage(image, forState: .Normal)
+        button.frame = CGRectMake(70, 200, 250, 250) // sets the location of the button
         
-        button.frame = CGRectMake(70, 200, 250, 250)
+        button.setTitle("Test Button", forState: UIControlState.Normal) // sets the title of the button
         
-        button.setTitle("Test Button", forState: UIControlState.Normal)
+        button.addTarget(self, action: #selector(AnimalsENViewController.buttonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside) // this line makes the button actionable button
+
         
-        button.addTarget(self, action: #selector(AnimalsENViewController.buttonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
-        
-        self.view.addSubview(button)
+        self.view.addSubview(button) // delegates to add sub view
         
         
         if languageChoice == 0 {
@@ -47,14 +46,14 @@ class AnimalsENViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-//  VARIABLES
+// !!== UI Outlet Objects == !!
     @IBOutlet weak var label: UILabel!
     
+// !!== VARIABLES == !!
     
-    let speechSynthesizer = AVSpeechSynthesizer()
+    let speechSynthesizer = AVSpeechSynthesizer() // creates a constant for text-to-speech
     
     var picsArray:[String] =  ["monkey.png","lion.png","snake.png","zebra.png","giraffe.png","elephant.png","crocodile.png","cat.png"]
     
@@ -69,10 +68,11 @@ class AnimalsENViewController: UIViewController {
     
     var randomNumber = arc4random_uniform(8)
     
-    let button = UIButton()
+    let button = UIButton() // creating button programmatically
     
-//  FUNCTIONS
+// !!== FUNCTIONS == !!
     
+    // function makes button clickable
     func buttonAction(sender:UIButton!)
     {
         
