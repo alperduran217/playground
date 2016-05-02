8537b55f97570f22b33d8802f977c825a3afe0fb
alperduran217 <alperduran217@gmail.com>
2016-05-02 17:34:47 +0300

    GamesEpisodesViewController comments are added.

--- Playground/GamesEpisodesViewController.swift
+++ Playground/GamesEpisodesViewController.swift
@@ -23,6 +23,7 @@ class GamesEpisodesViewController: UIViewController, UIScrollViewDelegate {
         
         timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(GamesEpisodesViewController.doAnimation), userInfo: nil, repeats: true)
         
+        //NSOBject that runs doAnimation2 function in every 0.1 seconds
         
         timer2 = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(GamesEpisodesViewController.doAnimation2), userInfo: nil, repeats: true)
 
@@ -35,20 +36,20 @@ class GamesEpisodesViewController: UIViewController, UIScrollViewDelegate {
     
     
     
-    var timer = NSTimer()
+    var timer = NSTimer() // NSObject for animations
     var timer2 = NSTimer()
-    var counterOperation:Int = 0
-    var counterAttention:Int = 0
+    var counterOperation:Int = 0 //This counter holds the frame of the animation Operation
+    var counterAttention:Int = 0 //This counter holds the frame of the animation Attention
     
-    @IBOutlet weak var attentionImage: UIImageView!
-    @IBOutlet weak var operationImage: UIImageView!
+    @IBOutlet weak var attentionImage: UIImageView! // UI Object where attention animation's first frame holds
+    @IBOutlet weak var operationImage: UIImageView! // UI Object where operation animation's first frame holds
     @IBOutlet weak var wordLabel: UILabel!
-    @IBOutlet weak var attentionLabel: UILabel!
-    @IBOutlet weak var operationsLabel: UILabel!
-    @IBOutlet weak var pageControl: UIPageControl!
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var attentionLabel: UILabel! // UI Object where attention label's text holds
+    @IBOutlet weak var operationsLabel: UILabel! // UI Object where operation label's text holds
+    @IBOutlet weak var pageControl: UIPageControl! // pageControl UI Object for navigate the page
+    @IBOutlet weak var scrollView: UIScrollView! // scrollView UI Object for slide the page on x-axis
     
-    
+    // translates the labels with the language choice variable's value
     func translator() {
         
         if languageChoice == 1 {
@@ -63,7 +64,7 @@ class GamesEpisodesViewController: UIViewController, UIScrollViewDelegate {
             operationsLabel.text = "Dört İşlem Oyunu"
         }
     }
-    
+    // it's not an animation function. It's just holds the information of the animation. timer(NS Object) does animate. doAnimation runs every n seconds where n is timer's value.
     func doAnimation() {
         if counterOperation == 23 {
             counterOperation = 1
