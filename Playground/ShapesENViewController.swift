785bc3b7dc80184f9785fb18f77454a6fb108581
alperduran217 <alperduran217@gmail.com>
2016-05-02 21:18:28 +0300

    ShapesENViewController comments are added

--- Playground/ShapesENViewController.swift
+++ Playground/ShapesENViewController.swift
@@ -12,24 +12,25 @@ import AVFoundation
 
 class ShapesENViewController: UIViewController {
     
-//  PRE-LOAD
+// !!== PRE-LOAD == !!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        
-        let image = UIImage(named: picsArray[Int(randomNumber)]) as UIImage?
+        let image = UIImage(named: picsArray[Int(randomNumber)]) as UIImage? // takes random image from array
 
-        button.setImage(image, forState: .Normal)
+
+        button.setImage(image, forState: .Normal) // sets button to that random image
         
-        button.frame = CGRectMake(70, 200, 250, 250)
+        button.frame = CGRectMake(70, 200, 250, 250) // sets the location of the button
+
         
-        button.setTitle("", forState: UIControlState.Normal)
+        button.setTitle("", forState: UIControlState.Normal) // sets the title of the button
         
-        button.addTarget(self, action: #selector(ShapesENViewController.buttonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        button.addTarget(self, action: #selector(ShapesENViewController.buttonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside) // this line makes the button actionable button
+
         
-        self.view.addSubview(button)
+        self.view.addSubview(button) // delegates to add sub view
         
         
         if languageChoice == 0 {
@@ -48,16 +49,20 @@ class ShapesENViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-//  VARIABLES
+    
+    // !!== UI Outlet Objects == !!
+    
     @IBOutlet weak var label: UILabel!
     
+    
+// !!== VARIABLES == !!
+  
     var namesArray:[String] = []
     
-    let button = UIButton() 
+    let button = UIButton() // creating button programmatically
     
-    let speechSynthesizer = AVSpeechSynthesizer()
+    let speechSynthesizer = AVSpeechSynthesizer() // creates a constant for text-to-speech
     
     var picsArray:[String] = ["parallelogram.png","star.png","rectangle.png","heart.png","square.png","triangle.png","oval.png","circle.png","trapezoid.png","deltoid.png","pentagon.png","hexagon.png"]
     
@@ -67,8 +72,9 @@ class ShapesENViewController: UIViewController {
     
     var randomNumber = arc4random_uniform(12)
     
-//  FUNCTIONS
+// !!== FUNCTIONS == !!
     
+    // function makes button clickable
     func buttonAction(sender:UIButton!)
     {
         
